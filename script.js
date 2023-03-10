8a630ebdcd959cb33aade8b91c4dac4b4f1c5762
EliaRenov <eliarenov04@gmail.com>
2023-03-10 09:38:31 +0200

    Add new game mechanics. Game script completed.
    Added a game function, calls the playRound function 5 times. Keeps score of each round. After 5 rounds, the winner is declared and the game is over.
    
    playRound: Function logs to the console the winner and loser of the round, and returns to the game function which player won the current round.

--- script.js
+++ script.js
@@ -12,33 +12,61 @@ function playRound(playerSelection, computerSelection = getComputerChoice()) {
     // If player chooses rock
     if (playerSelection === 'rock') {
         if (computerSelection === 'paper') {
-            return ('You Lose! Paper Beats Rock')
+            console.log('You Lose! Paper Beats Rock')
+            return 1
         } else if (computerSelection === 'scissors') {
-            return ('You Win! Rock beats Scissors')
+            console.log('You Win! Rock beats Scissors')
+            return 0
         } else {
-            return (`It's a Tie!`)
+            console.log(`It's a Tie!`)
+
         }
     }
     // If player chooses paper
     if (playerSelection === 'paper') {
         if (computerSelection === 'scissors') {
-            return ('You Lose! Scissors beat Paper')
+            console.log('You Lose! Scissors beat Paper')
+            return 1
         } else if (computerSelection === 'rock') {
-            return ('You Win! Paper beats Rock')
+            console.log('You Win! Paper beats Rock')
+            return 0
         } else {
-            return (`It's a Tie!`)
+            console.log(`It's a Tie!`)
+
         }
     }
     // If player chooses scissors
     if (playerSelection === 'scissors') {
         if (computerSelection === 'rock') {
-            return ('You Lose! Rock beats Scissors')
+            console.log('You Lose! Rock beats Scissors')
+            return 1
         } else if (computerSelection === 'paper') {
-            return ('You Win! Scissors beats Paper')
+            console.log('You Win! Scissors beats Paper')
+            return 0
         } else {
-            return (`It's a Tie!`)
+            console.log(`It's a Tie!`)
+
         }
     }
+}
 
+function game() {
+    // Score at index 0: Player
+    // Score at index 1: Computer
+    let scores = [0, 0]
+
+    // Loop iterates 5 times, meaning 5 rounds.
+    for (let i = 0; i < 5; i++) {
+        let playerMove = prompt('What move do you choose?')
+        let winner = playRound(playerMove, getComputerChoice())
+        scores[winner]++
+    }
+    if (scores[0] > scores[1]) {
+        console.log(`PLAYER WINS THE GAME!`)
+    } else if (scores[1] > scores[0]) {
+        console.log(`COMPUTER WINS THE GAME!`)
+    } else {
+        console.log(`THE GAME ENDS IN TIE`)
+    }
 
 }
